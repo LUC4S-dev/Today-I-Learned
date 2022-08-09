f8bda760cbbcb0a8dafee4beef325912108e99b4
Lucas Siqueira <lucaslenisbenicio@hotmail.com>
2022-08-09 08:53:36 -0300

    for in and of commit

---
+++ JavaScript/for in/forIn.js
@@ -0,0 +1,9 @@
+const pessoa = {
+nome : 'Lucas',
+altura: '2 metros',
+Idade: 16
+};
+
+for (let i in pessoa){
+    console.log(pessoa[i]);
+}
---
+++ JavaScript/for of/forOf.js
@@ -0,0 +1,5 @@
+const nome = 'irineu'
+    
+    for (let i in nome){
+        console.log(nome[i]);
+    }
